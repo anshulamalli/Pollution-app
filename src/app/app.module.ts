6b64490a240c13030c464976caae0306c2d8035b
unknown <bakiyaswanth2001@gmail.com>
2022-11-23 19:42:37 +0530

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import {MatToolbarModule, MatButtonModule, MatIconModule} from '@angular/material';
